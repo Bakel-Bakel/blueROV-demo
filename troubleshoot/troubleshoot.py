b4ab303cd6c389217a8954b9644a05d3d5446230
bakel-bakel <begededum4bakel@gmail.com>
2025-06-04 15:13:05 +0200

    suppress warnings

--- troubleshoot/troubleshoot.py
+++ troubleshoot/troubleshoot.py
@@ -5,12 +5,15 @@ from dronekit import connect
 import gii.gii_bluerov as gii_bluerov
 from gii.blueROV_config import get_config  # import the config getter
 
+from dronekit import VehicleMode
+
+
 # === Only Specify which BlueROV to use ===
 ROV_NAME = "bluerov2"  # Change this to match the blueROV
 config = get_config(ROV_NAME)
 
 # === Build connection string, DO NOT TOUCH ===
-connection_string = f"{config['rov_ip']}:{config['port']}"
+connection_string = f"{config['host_ip']}:{config['port']}"
 print(f"Connecting to vehicle on: {connection_string}")
 autopilot = connect(connection_string, wait_ready=False)
 
