b9841b288c95e82f9daeb78f7e5fa1c5db51102b
bakel-bakel <begededum4bakel@gmail.com>
2025-06-06 14:40:22 +0200

    added channel_maps

--- gii/blueROV_config.py
+++ gii/blueROV_config.py
@@ -1,17 +1,35 @@
-
 ROV_CONFIG = {
     "bluerov1": {
         "host_ip": "192.168.3.10",
         "rov_ip": "192.168.3.11",
         "port": 14551,
-        "gripper": False
+        "gripper": False,
+        "channel_map": {
+            1: "rotate_y",
+            2: "rotate_x",
+            3: "move_z",
+            4: "rotate_z",
+            5: "move_x",
+            6: "move_y",
+            9: "lights"
+        }
     },
     "bluerov2": {
         "host_ip": "192.168.3.20",
         "rov_ip": "192.168.3.21",
         "port": 14552,
         "gripper": True,
-        "gripper_channel": 7
+        "gripper_channel": 7,
+        "channel_map": {
+            1: "rotate_y",
+            2: "rotate_x",
+            3: "move_z",
+            4: "rotate_z",
+            5: "move_x",
+            6: "move_y",
+            7: "gripper",
+            9: "lights"
+        }
     }
 }
 
@@ -21,4 +39,3 @@ def get_config(rov_name=None):
     if rov_name is None:
         rov_name = DEFAULT
     return ROV_CONFIG.get(rov_name.lower())
-
